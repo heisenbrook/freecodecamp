5d0c1bc159b12f8057af87c260f3fdee3a381b47
heisenbrook <136280542+heisenbrook@users.noreply.github.com>
2023-07-05 01:01:37 +0200

    Update budget.py

--- Budget app/budget.py
+++ Budget app/budget.py
@@ -1,3 +1,8 @@
+def create_spend_chart(cat_list):
+    return None
+
+
+
 class Category:
     def __init__(self, category):
         self.category = category
@@ -5,34 +10,26 @@ class Category:
         self.balance = 0.0
         pass
 
-    def __repr__(self):
+    def __str__(self):
         title = self.category.center(30,'*')
-        ledger = list()
+        ledger = str()
         for line in self.ledger:
-            ledger.append(f"{line['amount']:< 23}{line['description']:> 7}")
-        total = 'Total:' + str(round(self.balance,2))    
-        return title,'\n',ledger,'\n',total
-
-    def check_funds(self, amount):
-        if amount > self.balance:
-            return False
-        else:
-            return True
+            ledger += (f"{line['description'][0:23]:23}"+f"{line['amount']:>7.2f}") +'\n'
+        total = 'Total:' + str(self.balance)   
+        return title + '\n' + ledger + total
    
     def deposit(self, amount, description =' '):
+        self.balance += amount
         status = self.check_funds(amount)
         if status == True:
-            self.balance += amount
-            self.ledger['amount'].append(round(float(amount),2))
-            self.ledger['description'].append(description)
+            self.ledger.append({"amount": amount, "description": description})
 
 
     def withdraw(self, amount, description =' '):
+        self.balance -= amount
         status = self.check_funds(amount)
         if status == True:    
-            self.balance -= amount
-            self.ledger['amount'].append(round(float(-amount),2))
-            self.ledger['description'].append(description)
+            self.ledger.append({"amount": -amount, "description": description})
             return True
         else:
             return False
@@ -41,15 +38,18 @@ class Category:
         return self.balance
     
     def transfer(self, amount, category_new):
-        self.balance -= amount
         if self.balance > 0:
-            self.ledger['amount'].append(round(float(amount),2))
-            self.ledger['description'].append('Transfer to' + category_new.balance)
-            self.ledger['amount'].append(round(float(-amount),2))
-            self.ledger['description'].append('Transfer from' + self.balance)
+            self.withdraw(amount, 'Transfer to ' + category_new.category)
+            category_new.deposit(amount, 'Transfer from ' + self.category)
             return True
         else:
             return False
+    
+    def check_funds(self, amount):
+        if amount > self.balance:
+            return False
+        else:
+            return True
         
 
 

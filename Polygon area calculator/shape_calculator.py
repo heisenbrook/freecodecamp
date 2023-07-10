41540d079b71cc49554041f7439eb6a1bcd87632
heisenbrook <136280542+heisenbrook@users.noreply.github.com>
2023-07-11 01:37:49 +0200

    Update shape_calculator.py

--- Polygon area calculator/shape_calculator.py
+++ Polygon area calculator/shape_calculator.py
@@ -1,5 +1,27 @@
 class Rectangle:
-
+    
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        pass
+    
+    def set_width(self, width):
+        self.width += width
+    
+    def set_height(self, height):
+        self.height += height
+    
+    def get_area(self):
+        area = self.height * self.width
+        return area
+    
+    def get_perimeter(self):
+        perimeter = (2 *self.width) + (2*self.height)
+        return perimeter
+    
+    def get_diagonal(self):
+        diagonal = ((self.width**2)+(self.height**2))**.5
+        return diagonal
 
 
 

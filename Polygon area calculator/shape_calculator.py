65747d28c32ef6e31717b78f669fd882658fd4ba
heisenbrook <136280542+heisenbrook@users.noreply.github.com>
2023-07-11 16:16:39 +0200

    Update shape_calculator.py

--- Polygon area calculator/shape_calculator.py
+++ Polygon area calculator/shape_calculator.py
@@ -22,7 +22,26 @@ class Rectangle:
     def get_diagonal(self):
         diagonal = ((self.width**2)+(self.height**2))**.5
         return diagonal
+    
+    def get_picture(self):
+        i = 1
+        line = str()
+        if self.height <= 50 or self.width <= 50:
+            line += (f'Rectangle(width={self.width}, height={self.height}) \n')
+            for i in range(self.height):
+                line += ('*'*self.width) + '\n'
+            return line
+        else: 
+            return 'Too big for picture.'
+        
+    def get_amount_inside(self):
+        return
+    
 
 
 
-class Square:
+
+
+
+
+class Square(Rectangle):

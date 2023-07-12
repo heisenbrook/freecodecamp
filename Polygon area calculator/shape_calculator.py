6720f863bd09191fe041909336246da601035350
heisenbrook <136280542+heisenbrook@users.noreply.github.com>
2023-07-12 02:22:01 +0200

    Update shape_calculator.py

--- Polygon area calculator/shape_calculator.py
+++ Polygon area calculator/shape_calculator.py
@@ -4,7 +4,13 @@ class Rectangle:
         self.width = width
         self.height = height
         pass
-    
+
+    def __str__(self):
+        line = str()
+        if self.height <= 50 or self.width <= 50:
+            line += (f'Rectangle(width={self.width}, height={self.height}) \n')
+        return line
+
     def set_width(self, width):
         self.width += width
     
@@ -27,21 +33,20 @@ class Rectangle:
         i = 1
         line = str()
         if self.height <= 50 or self.width <= 50:
-            line += (f'Rectangle(width={self.width}, height={self.height}) \n')
             for i in range(self.height):
                 line += ('*'*self.width) + '\n'
             return line
         else: 
             return 'Too big for picture.'
         
-    def get_amount_inside(self):
-        return
+    def get_amount_inside(self, shape):
+        return self.get_area() // shape.get_area()
     
 
 
-
-
-
-
-
 class Square(Rectangle):
+    def __init__(self, width):
+        super().__init__(width)
+        self.height = width
+        self.width = width
+    
